7a49a54fbe494eae497c1716495f9cefeb84ae2a
Rafae1TCC <rafael.cabrera32@uabc.edu.mx>
2026-06-02 18:14:45 -0700

    Initial Commit

---
+++ helllo world/helllo_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helllo_worldApp.swift
+//  helllo world
+//
+//  Created by Rafael Cabrera on 6/2/26.
+//
+
+import SwiftUI
+
+@main
+struct helllo_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
