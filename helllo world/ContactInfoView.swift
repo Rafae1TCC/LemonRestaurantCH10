7de0005ff2db635407fed3e7ab8f7f517b6f2ed9
Rafael Cabrera <rafael.cabrera32@uabc.edu.mx>
2026-06-20 00:30:59 -0700

    101 Class #3 - 102 Class #3

---
+++ helllo world/ContactInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  ContactInfoView.swift
+//  helllo world
+//
+//  Created by Rafael Cabrera on 6/13/26.
+//
+
+import SwiftUI
+
+struct ContactInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactInfoView()
+}
---
+++ helllo world/RestaurantInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantInfoView.swift
+//  helllo world
+//
+//  Created by Rafael Cabrera on 6/16/26.
+//
+
+import SwiftUI
+
+struct RestaurantInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RestaurantInfoView()
+}
